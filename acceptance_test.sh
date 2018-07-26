f4bf00fa796ea8e0df15fdb7ad6767f6b30227aa
root <root@quercus.elbor.org>
2018-07-26 10:26:49 +0200

    Added acceptance tests

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 CALCULATOR_PORT=$(docker-compose port calculator 9080 | cut -d: -f2)
-test $(curl localhost:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
+#test $(curl localhost:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
+test $(./gradlew acceptanceTest -Dcalculator.url=http://localhost:9080) -eq 3
